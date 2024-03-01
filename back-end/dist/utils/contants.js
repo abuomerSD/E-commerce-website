ca4ab84bd306cd971f35bcc8fb45049947297013
asdf <tayeb1293@yahoo.com>
2024-03-01 21:09:12 +0200

    feature: categories pagination

--- back-end/dist/utils/contants.js
+++ back-end/dist/utils/contants.js
@@ -2,4 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DEFAULT_PAGE_LIMIT = exports.DEFAULT_PAGE_NUMBER = void 0;
 exports.DEFAULT_PAGE_NUMBER = 1;
-exports.DEFAULT_PAGE_LIMIT = 5;
+exports.DEFAULT_PAGE_LIMIT = 10;
