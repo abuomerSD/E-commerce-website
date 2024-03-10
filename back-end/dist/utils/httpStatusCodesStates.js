cb459afc248ad46ffdfee9493de0c8b17d68607f
Eltayeb Ibrahim <80187951+abuomerSD@users.noreply.github.com>
2024-03-10 14:24:15 +0200

    Delete back-end/dist directory

--- back-end/dist/utils/httpStatusCodesStates.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.httpStatus = void 0;
-exports.httpStatus = {
-    SUCCESS: "success",
-    FAIL: "fail",
-    ERROR: "error",
-};
