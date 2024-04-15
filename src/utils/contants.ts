6556f958f0cd9624cfd9eef2471bc0c907c5e944
asdf <tayeb1293@yahoo.com>
2024-04-15 10:40:14 +0200

    fix: date error in purchase and sales invoices list

--- src/utils/contants.ts
+++ src/utils/contants.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_PAGE_NUMBER = 1;
-export const DEFAULT_PAGE_LIMIT = 10;
+export const DEFAULT_PAGE_LIMIT = 5;
 export const BEST_SELLERS_LIMIT = 9 ;
 export const WEBSITE_NAME: string = 'Step Shopping';
 export const MAX_AGE = 1 * 24 * 60 * 60;
